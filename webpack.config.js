f5e3480998560e025715053e6179ce8023b7c49e
Liam Bennett <liabennett@gumtree.com>
2019-09-06 23:36:08 +0100

    [upgrade-test] Added file and url loader for images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,56 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	cache: true,
-	devtool: 'source-map',
+	devtool: "source-map",
 	devServer: {
-		inline: true,
+		inline: true
 	},
 	resolve: {
-		extensions: ['.js']
+		extensions: [".js"]
 	},
 	entry: {
 		index: ["./src/js/index.js"]
 	},
 	output: {
 		path: path.resolve(__dirname, "build"),
-		filename: '[name].js',
-		sourceMapFilename: '[file].map'
+		filename: "[name].js",
+		sourceMapFilename: "[file].map"
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'public', 'index.html'),
-			favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+			template: path.join(__dirname, "public", "index.html"),
+			favicon: path.resolve(__dirname, "public", "favicon.ico")
 		}),
 		new MiniCssExtractPlugin({
-			filename: '[name].css'
+			filename: "[name].css"
 		})
 	],
 	module: {
-		rules: [{
-			test: /\.s?css$/,
-			use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
-		},{
-			test: /\.js$/,
-			exclude: [/node_modules/],
-			use: ['babel-loader', 'eslint-loader']
-		}]
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+			},
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: ["babel-loader", "eslint-loader"]
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							limit: 8192
+						}
+					}
+				]
+			}
+		]
 	}
 };
